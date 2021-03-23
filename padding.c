1e3f7b297c67efc5beed74871447bf30cec79901
Minseonhong <80440122+Minseonhong@users.noreply.github.com>
2021-03-24 00:43:06 +0900

    Update padding.c

--- padding.c
+++ padding.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 
 struct student{
-	char lastName[13];
-	int studentId;
-	short grade;
+	char lastName[13]; // 13바이트
+	int studentId; // 4바이트
+	short grade; // 2
 }; // 변수 3개를 갖는 구조체 student를 선언
 
 int main()
