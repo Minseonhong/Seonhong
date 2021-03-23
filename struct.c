3b9328cf6c8c54ef513fdab26f24f6dacf6017fa
Minseonhong <80440122+Minseonhong@users.noreply.github.com>
2021-03-24 00:33:49 +0900

    Update struct.c

--- struct.c
+++ struct.c
@@ -17,23 +17,23 @@ int main(){
 	struct student1 st1 = {'A', 100, 'A'}; // student1 자료형을 갖는 변수 st1에 A,100,A를 대입
 
 	printf("[----- [민선홍] [2018038028] -----\n]");
-	printf("st1.lastName = %c\n", st1.lastName); // st1변수에 저장된 lastName을 출력
-	printf("st1.studentId = %d\n", st1.studentId); // st1변수에 저장된 studentId를 출력
-	printf("st1.grade = %c\n", st1.grade); // st1변수에 저장된 grade를 출력
+	printf("st1.lastName = %c\n", st1.lastName); // 멤버연산자.을 이용하여 st1변수에 저장된 lastName을 출력
+	printf("st1.studentId = %d\n", st1.studentId); // 멤버연산자.을 이용하여 st1변수에 저장된 studentId를 출력
+	printf("st1.grade = %c\n", st1.grade); // 멤버연산자.을 이용하여 st1변수에 저장된 grade를 출력
 
 	student2 st2 = {'B', 200, 'B'}; // stdent2 자료형을 갖는 변수 st2에 B,200,B를 대입. (typedef를 선언하였기 때문에 struct 쓸 필요 x)
 
-	printf("\nst2.lastName = %c\n", st2.lastName); // st2변수에 저장된 lastName을 출력
-	printf("st2.studentId = %d\n", st2.studentId); // st2변수에 저장된 studentId를 출력
-	printf("st2.grade = %c\n", st2.grade); // student2의 grade를 출력
+	printf("\nst2.lastName = %c\n", st2.lastName); // 멤버연산자.을 이용하여 st2변수에 저장된 lastName을 출력
+	printf("st2.studentId = %d\n", st2.studentId); // 멤버연산자.을 이용하여 st2변수에 저장된 studentId를 출력
+	printf("st2.grade = %c\n", st2.grade); // 멤버연산자.을 이용하여 st2변수에 저장된 grade를 출력
 
 
 	student2 st3; // student2 자료형을 갖는 변수 st3를 선언
 
 	st3 = st2; // 변수 st3에 st2를 대입(구조치환을 사용하여 가능)
-	printf("\nst3.lastName = %c\n", st3.lastName);// st3변수에 저장된 lastName을 출력 = st2.lastName
-	printf("st3.studentId = %d\n", st3.studentId);// st3변수에 저장된 studentId를 출력 = st2.studentId
-	printf("st3.grade = %c\n", st3.grade);// st3변수에 저장된 grade를 출력 = st2.grade
+	printf("\nst3.lastName = %c\n", st3.lastName);// 멤버연산자.을 이용하여 st3변수에 저장된 lastName을 출력 = st2.lastName
+	printf("st3.studentId = %d\n", st3.studentId);// 멤버연산자.을 이용하여 st3변수에 저장된 studentId를 출력 = st2.studentId
+	printf("st3.grade = %c\n", st3.grade);// 멤버연산자.을 이용하여 st3변수에 저장된 grade를 출력 = st2.grade
 
 	/* equality test
 	if(st3==st2) // 사용할수 없다
